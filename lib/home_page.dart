9e2a771f61dd0e9005b145e4419bd596dfef2391
= <parshvashah414@gmail.com>
2019-10-01 19:02:51 -0400

    Added to UI

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,30 +9,43 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Calculator"),
-      ),
-      body: new Container(
-        padding: const EdgeInsets.all(20.0),
-        child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new TextField(
-                keyboardType: TextInputType.number,
-                decoration: new InputDecoration(hintText: "Enter Number 1"),
-              ),
-              new TextField(
-                keyboardType: TextInputType.number,
-                decoration: new InputDecoration(hintText: "Enter Number 1"),
-              ),
-              new Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-              ),
-              new Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-              )
-            ]),
-      ),
-    );
+        appBar: new AppBar(
+          title: new Text("Calculator"),
+        ),
+        body: new Container(
+            padding: const EdgeInsets.all(20.0),
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: new InputDecoration(hintText: "Enter Number 1"),
+                ),
+                new TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: new InputDecoration(hintText: "Enter Number 1"),
+                ),
+                new Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                ),
+                new Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      new RaisedButton(
+                        child: new Text("+"),
+                        onPressed: () {},
+                      ),
+                      new RaisedButton(
+                        child: new Text("-"),
+                        onPressed: () {},
+                      ),
+                      new RaisedButton(
+                        child: new Text("*"),
+                        onPressed: () {},
+                      ),
+                      new RaisedButton(child: new Text("/"), onPressed: () {}),
+                    ])
+              ],
+            )));
   }
 }
